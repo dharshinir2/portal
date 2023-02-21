38c12d736b8f45ec0f7a5b9a35d39a8c96694af1
DHARSHINI R <727721euai017@skcet.ac.in>
2023-02-21 12:48:11 +0530

    upload

---
+++ demo/src/main/java/com/example/demo/Api.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Api {
+	private String message;
+	public Api(String message) {
+		this.message=message;
+	}
+public String getMessage() {
+	return message;
+}
+
+}
---
+++ example/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Student {
+	
+	private int rollno;
+	@JsonProperty("id")
+	private String name;
+	@JsonIgnore
+	private int rank;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student(int rollno, String name, int rank) {
+		super();
+		this.rollno = rollno;
+		this.name = name;
+		this.rank = rank;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	
+
+}
